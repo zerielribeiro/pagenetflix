beb87fa7ddc845251b1bffbba548bace68b28b68
zeriel <zerielr@gmail.com>
2020-11-13 12:07:42 -0300

    criando page netflix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, {useEffect, useState}from 'react';
+
+import Tmdb from './tmdb';
 import './App.css';
+import MovieRow from './components/MovieRow';
+import FeaturedMovir from './components/FeaturedMovie';
+import FeaturedMovie from './components/FeaturedMovie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default () => {
+    
+    const[movieList, setMovieList] = useState([]);
+    const[featuredData, setFeaturedData] = useState(null);
+
+ useEffect(()=>{
+     const loadAll = async () =>{
+          //pegando a lista total de filmes
+     let list = await Tmdb.getHomeList();
+    setMovieList(list);
+
+    // pegando o feature em destaque
+    let originals = list.filter(i=>i.slug === 'originals');
+    //gerando um filme aleatorio de todos os originais
+    let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+    // pegando o filme escolhido
+    let chosen = originals[0].items.results[randomChosen];
+    let chosenInfo= await Tmdb.getMovieInfo(chosen.id, 'tv');
+    setFeaturedData(chosenInfo);
+
+
+     }
+
+     loadAll();
+        
+ },[]);
+
+return(
+    <div className='pages'>
+
+        {featuredData && 
+        <FeaturedMovie item={featuredData}
+        />}
+
+        <section className='list'>
+        {movieList.map((item, key)=>(
+            <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+
+        </section>
+
+
     </div>
-  );
-}
+);
 
-export default App;
+}
